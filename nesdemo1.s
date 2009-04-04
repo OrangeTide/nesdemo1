56a7fe88e3922895b7fe5deb9045fae63c155cfb
Jon Mayo <jon.mayo@gmail.com>
2009-04-04 00:56:43 -0700

    macros and definitions for NES.

--- nesdemo1.s
+++ nesdemo1.s
@@ -1,3 +1,5 @@
+.include "util.inc"
+.include "nesdefs.inc"
 .segment "CODE"
 
 nmi_vect:
@@ -10,14 +12,18 @@ reset_vect:
 ;;; init cpu
 ; TODO
 
-;;; wait for vblank
-; TODO
+;;; wait for first vblank
+vblankwait1:
+	lda PPU_STATUS
+	bpl vblankwait1
 
 ;;; clear ram
 ; TODO
 
-;;; wait for vblank
-; TODO
+;;; wait for second vblank
+vblankwait2:
+	lda PPU_STATUS
+	bpl vblankwait2
 
 loop1:
 	jmp loop1
