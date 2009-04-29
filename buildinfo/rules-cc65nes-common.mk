c63e8627e73d09b59c25b6728216267b17f8996b
Jon Mayo <jon.mayo@gmail.com>
2009-04-29 00:48:01 -0700

    fixed up build system to generate CHR image.

--- buildinfo/rules-cc65nes-common.mk
+++ buildinfo/rules-cc65nes-common.mk
@@ -2,10 +2,14 @@ AS.target=ca65
 LD.target=ld65
 
 %.nes : $(TARGET_DIR)/%.prg %.chr
-	cat $^ >$@
+	cp $(filter %.prg,$^) $@
+	dd if=$(filter %.prg,$^) of=$@ conv=notrunc bs=16400 seek=1
 
 %.prg : %.o
 	$(LD.target) -o $@ -C nes.lds $^
+
+%.chr : %.png
+	pngtochr -o $@ $^
 
 $(TARGET_DIR)/%.o : %.s
 	$(AS.target) -o $@ $^
--- nesdemo1.s
+++ nesdemo1.s
@@ -2,15 +2,33 @@
 .include "nesdefs.inc"
 .segment "CODE"
 
+; write A to PPU and its mirror
+write_ppu_reg1:
+	sta PPU_CTRL_REG1
+	sta PPU_CTRL_REG1
+	rts
+
 nmi_vect:
+	lda PPU_STATUS
+	pla
+	ora #127
+	sta PPU_CTRL_REG1
 	rti
 
 irq_vect:
 	rti
 
 reset_vect:
-;;; init cpu
-; TODO
+
+; init cpu
+	sei
+	cld	; doesn't do anything on a real NES cpu
+; init PPU REG1
+	lda #16
+	sta PPU_CTRL_REG1
+; reset stack pointer
+	ldx #255
+	txs
 
 ;;; wait for first vblank
 vblankwait1:
@@ -24,6 +42,11 @@ vblankwait1:
 vblankwait2:
 	lda PPU_STATUS
 	bpl vblankwait2
+
+;;; we could do a cold boot / warm boot check here
+
+	ora #127
+	sta PPU_CTRL_REG1
 
 loop1:
 	jmp loop1
