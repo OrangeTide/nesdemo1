afbcb16f1eedc5681b34843ad2995637fc5398e3
Jon Mayo <jon.mayo@gmail.com>
2009-07-07 17:27:09 -0700

    some changes, i forget what.

--- nesdemo3.s
+++ nesdemo3.s
@@ -1,21 +1,26 @@
-; game3.s - Jon Mayo - May 4, 2009
+;nesdemo3.s - Jon Mayo - May 4, 2009
 ;
 
 .include "nesdefs.inc"
 .include "util.inc"
+.include "rand.inc"
 
 .export _start, _nmi_vect, _irq_vect
 
 .segment "CODE"
 
 .proc _start
+	lda #50
+	sta ScrollY
+	lda #1
+	sta ScrollDir
 
 	; set PPU to palette RAM
 	lda #$3f
 	sta PPU_ADDRESS
 	lda #0
 	sta PPU_ADDRESS
-	
+
 	; load first 4 colors
 	lda #$0f ; black (don't use $0d)
 	sta PPU_DATA
@@ -34,8 +39,8 @@
 	lda #0
 	sta PPU_ADDRESS
 
-	lda #22 ; copy to each entry
-	jsr fill_name_table 
+	lda #'g'
+	jsr fill_name_table
 
 	; set PPU to Name Table #2
 	lda #$28
@@ -43,8 +48,8 @@
 	lda #0
 	sta PPU_ADDRESS
 
-	lda #14 ; copy to each entry
-	jsr fill_name_table 
+	lda #'e'
+	jsr fill_name_table
 
 	WaitForVBlank ; wait for vblank before enabling display
 
@@ -59,7 +64,7 @@
 	sta PPU_CTRL_REG2
 
 	; scroll to top left
-	lda #0 
+	lda #0
 	sta PPU_SCROLL_REG
 	sta PPU_SCROLL_REG
 
@@ -99,7 +104,47 @@ _nmi_vect:
 	sta PPU_SCROLL_REG
 	lda ScrollY ; vertical
 	sta PPU_SCROLL_REG
-	inc ScrollY
+
+	; add in a direction
+	lda ScrollDir
+	adc ScrollY
+	sta ScrollY
+
+	; test if ScrollY outside of the range of 50 and 100
+	cmp #50
+	bcc :+
+	lda #51
+	sta ScrollY
+	lda #1
+	sta ScrollDir
+	jmp @do_reload_name_table
+:	; else
+	lda ScrollY
+	cmp #100
+	bcc :+
+	beq :+
+	; reset ScrollY to 99
+	lda #100
+	sta ScrollY
+	lda #255
+	sta ScrollDir
+	jmp @do_reload_name_table
+:	; out
+
+
+	jmp @skip_reloading_name_table ; skip over reloading name table
+@do_reload_name_table:
+	; set PPU to Name Table #0
+	lda #$20
+	sta PPU_ADDRESS
+	lda #0
+	sta PPU_ADDRESS
+
+	jsr rand ; pick a random value
+	lda #'m'
+	jsr fill_name_table
+
+@skip_reloading_name_table:
 
 	pla ; restore A
 	rti
@@ -110,6 +155,7 @@ _irq_vect:
 .segment "ZEROPAGE", zeropage
 ScrollX = 0
 ScrollY = 0
+ScrollDir = 0
 
 .segment "RODATA"
 Name_table_entries = 960
